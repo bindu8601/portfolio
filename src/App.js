5dc8f2518abc0646dd129100cd9bc37d6eaf4eb5
bindu8601 <127506733+bindu8601@users.noreply.github.com>
2025-01-19 17:56:59 +0530

    bug fixes

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { About } from "./components/About";
 import { Contact } from "./components/Contact";
-import { Footer } from "./components/Footer";
 import { Header } from "./components/Header";
 import { MainSection } from "./components/Mainsection";
-import { Painting } from "./components/Paintings";
 import { Projects } from "./components/Projects";
 import { Skills } from "./components/Skills";
 import "./style.css";
@@ -14,10 +12,8 @@ function App() {
       <MainSection />
       <Skills />
       <Projects />
-      {/* <Painting /> */}
       <About />
       <Contact />
-      {/* <Footer /> */}
     </div>
   );
 }
